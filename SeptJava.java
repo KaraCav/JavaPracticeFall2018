89d55e3a03a3a2ab4cf9e98dea28050da70244ec
Kara Cavanaugh <karacav@msn.com>
2018-09-30 23:32:34 -0400

    Example 2- chaining functions

--- SeptJava.java
+++ SeptJava.java
@@ -14,3 +14,36 @@ public class BreakContinue {
           System.out.println("done");
   }  
 }
+
+// 2. Chaining Functions
+public class Absolute {
+  public static void main(String[] args) {
+    int numberOne = -10;
+    System.out.println(numberOne);
+    int numberTwo = 5;
+    int returnAbs = abs(numberTwo);
+    System.out.println(returnAbs);
+    abs(numberOne);
+  }
+  public static int abs(int arg1) {
+    if (arg1 < 0) {
+      arg1 = -arg1;
+    }
+      return arg1;
+  }
+  public static int add(int arg1, int arg2){
+        System.out.println(arg1 + arg2);
+   return arg1 + arg2; 
+  }
+  public static int larger(int arg1, int arg2){
+    if (arg1 > arg2){
+     return arg1; 
+    }
+    else{
+    return arg2;
+    }
+  }
+  public static int compareThree(int arg1, int arg2, int arg3){
+   return larger(arg1, larger(arg2, arg3)); 
+  }
+}
