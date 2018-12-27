684ef5c7356c507bf051909a99208de5d6edc240
Kara Cavanaugh <karacav@msn.com>
2018-12-27 11:31:50 -0500

    MIT Lecture 1

---
+++ Dec.java
@@ -0,0 +1,8 @@
+// MIT Courseware Introduction to Computers and Engineering Problem Solving
+public class NauticalMile {
+	public static void main(String[] args) {
+		double circum = 24859.82 * 5280; // double is floating point type, Real
+		in minutesInCircle = 360*60;
+		double nautMile = circum / minutesInCircle;
+	}
+}
